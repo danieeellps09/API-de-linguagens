bfb08f513c8d69b52d3b0381d8ea0e3e55afac07
daniel lopes <danieltricolorlps@gmail.com>
2022-08-08 22:32:58 -0300

    erro de chave resolvido

--- src/main/java/com/example/CriacaoAPI/LinguagemController.java
+++ src/main/java/com/example/CriacaoAPI/LinguagemController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 public class LinguagemController {
     @Autowired // Anotação autowired
-    private  LinguagemRepository repository;
+    private LinguagemRepository repository;
 
    /* private List <Linguagem> linguagens =
             List.of(
@@ -20,19 +20,19 @@ public class LinguagemController {
                    new Linguagem( "PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
             );*/
 
-   @GetMapping("/linguagens")
-    public List<Linguagem> obterLinguagens (){
-       List <Linguagem> linguagens = repository.findAll(); // Pegar essa lista do Banco de Dados
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens() {
+        List<Linguagem> linguagens = repository.findAll(); // Pegar essa lista do Banco de Dados
         return linguagens;
-   }
+    }
 
 
-
-   //Requisição Postman
-    @PostMapping ("/linguagens")
-    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem){
+    //Requisição Postman
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
         Linguagem linguagemSalva = repository.save(linguagem);
         return linguagemSalva;
 
 
+    }
 }
